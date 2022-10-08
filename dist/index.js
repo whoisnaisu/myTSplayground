82f878d7122bc71b7d3211efb956ad9db5af7c0d
naisu <napas14204@gmaill.com>
2022-10-08 19:13:20 +0700

    enum

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 1] = "Medium";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// PascalCase
+enum Size {
+  Small,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+console.log(mySize);
