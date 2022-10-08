5a9d04c239c5509a6b61709e49ab26d4b3deab67
naisu <napas14204@gmaill.com>
2022-10-08 22:10:19 +0700

    function annotated

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,14 @@ enum Size {
 }
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+// function
+// the best practice is should always properly annotated functions, like parameters and return type. Esp when building API for other people
+function calculateTax(income: number, taxYear: number): number {
+  if (taxYear < 2022) return income * 1.2;
+  return income * 1.3;
+}
+
+// :void if the function are not going to return a value
+
+calculateTax;
